c9b882a1b7568535bdfc3b70d0af50ffdc852224
gerryhubai <geremaineemefa@gmail.com>
2024-09-09 03:44:22 +0100

    coded referral aspect

--- components/ReferralSystem.tsx
+++ components/ReferralSystem.tsx
@@ -10,7 +10,7 @@ interface ReferralSystemProps {
 const ReferralSystem: React.FC<ReferralSystemProps> = ({ initData, userId, startParam }) => {
   const [referrals, setReferrals] = useState<string[]>([])
   const [referrer, setReferrer] = useState<string | null>(null)
-  const INVITE_URL = "https://t.me/referral_showcase_bot/start"
+  const INVITE_URL = "https://t.me/telemas_ai_bot/Farm"
 
   useEffect(() => {
     const checkReferral = async () => {
@@ -49,7 +49,7 @@ const ReferralSystem: React.FC<ReferralSystemProps> = ({ initData, userId, start
   const handleInviteFriend = () => {
     const utils = initUtils()
     const inviteLink = `${INVITE_URL}?startapp=${userId}`
-    const shareText = `Join me on this awesome Telegram mini app!`
+    const shareText = `Join me accumulate coins for the first christmas airdop bot on Telegram.`
     const fullUrl = `https://t.me/share/url?url=${encodeURIComponent(inviteLink)}&text=${encodeURIComponent(shareText)}`
     utils.openTelegramLink(fullUrl)
   }
@@ -57,7 +57,6 @@ const ReferralSystem: React.FC<ReferralSystemProps> = ({ initData, userId, start
   const handleCopyLink = () => {
     const inviteLink = `${INVITE_URL}?startapp=${userId}`
     navigator.clipboard.writeText(inviteLink)
-    alert('Invite link copied to clipboard!')
   }
 
   return (
