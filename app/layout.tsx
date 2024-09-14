49fb56e339050c9dfda0034e86ab9781e4f50a84
gerryhubai <geremaineemefa@gmail.com>
2024-09-14 06:47:33 +0100

    added adsgram cdn code to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientLayout from './ClientLayout';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script src="https://cdn.adsgram.ai/adsgram-sdk.min.js" />
+      </head>
       <body className={inter.className}>
         <ClientLayout>{children}</ClientLayout>
       </body>
