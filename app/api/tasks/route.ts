410d4e5b13936abc3255fca12939fa68ac4de68e
gerryhubai <geremaineemefa@gmail.com>
2024-09-15 13:37:06 +0100

    Update route.ts

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -2,8 +2,16 @@ import { NextResponse } from 'next/server';
 
 const tasks = [
   {
+    id: 0,
+    title: "Play Dapp Farm",
+    description: "Play, learn and earn with our partners",
+    link: "https://t.me/DappFarmBot/earn",
+    reward: 1500,
+    emoji: "🎮"
+  },
+  {
     id: 7,
-    title: "Join Telegram Channel",
+    title: "Join our Telegram Channel",
     description: "Join our Telegram channel for the latest updates!",
     link: "https://t.me/telemasgold",
     reward: 1000,
@@ -11,7 +19,7 @@ const tasks = [
   },
   {
     id: 8,
-    title: "Follow on Twitter",
+    title: "Follow us on Twitter",
     description: "Follow us on Twitter for quick updates and news!",
     link: "https://twitter.com/telemas_ai_bot",
     reward: 1500,
@@ -19,21 +27,13 @@ const tasks = [
   },
   {
     id: 9,
-    title: "Subscribe on YouTube",
+    title: "Subscribe to our YouTube",
     description: "Subscribe to our YouTube channel for exciting videos!",
     link: "https://www.youtube.com/@telemasgold",
     reward: 1500,
     emoji: "🎥"
   },
-  {
-    id: 10,
-    title: "Play Dapp Farm",
-    description: "Play, learn and earn with our partners",
-    link: "https://t.me/DappFarmBot/earn",
-    reward: 1500,
-    emoji: "🎥"
-  },
-];
+  ];
 
 export async function GET() {
   return NextResponse.json(tasks);
